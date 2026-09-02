9e7891d7df20ee6ceff90023544fe746e72c94b6
Andy Iakovlev <andy.iakovlev@adjoe.io>
2026-09-02 11:36:45 +0000

    Update PlaytimeMonetize to version 4.5.4

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PlaytimeMonetize",
-            url: "https://releases.adjoe.io/files/playtime/ios/monetize/5.0.0-beta.1/PlaytimeSDK.zip",
-            checksum: "3707cf33a075a181e93e8132a48b606fc5fa9737e768928338e8a4824db132d7"
+            url: "https://releases.adjoe.io/files/playtime/ios/monetize/4.5.4/PlaytimeMonetize.zip",
+            checksum: "d121a300406c71e76e0a38fa9185f6b7067f404c3f92e29a5345060600321cbd"
         )
     ]
 )
